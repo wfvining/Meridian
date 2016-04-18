dbf25720891a11323bdbb256a0eb7ddd81fc720e
Will Vining <wfvining@gmail.com>
2016-04-18 17:57:16 -0600

    removed unnecessary record declaration and added default number of seeds

--- include/map_elites.hrl
+++ include/map_elites.hrl
@@ -1,11 +1,8 @@
 -type genotype()  :: term().
 -type phenotype() :: {genotype(), term()}.
 
--record(mape, {map, granularity,
-	       grid_index=ets:new(grid_index, [set, public]),
-	       map_index=ets:new(map_index, [set, public])}).
-
 -define(DEFAULT_GRANULARITY, 256).
 -define(DEFAULT_MAP_NAME, 'unnamed_mape').
 -define(DEFAULT_ITERATIONS, 5000).
+-define(DEFAULT_SEEDS, 5).
 
