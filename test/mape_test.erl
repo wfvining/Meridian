dffa5c7b1d8966d91c2250b3b6aa561ffb974c56
Will Vining <wfvining@gmail.com>
2016-05-19 15:50:13 -0600

    basic archive tests

--- test/mape_test.erl
+++ test/mape_test.erl
@@ -2,25 +2,12 @@
 -include_lib("eunit/include/eunit.hrl").
 
 %% TODO:
-%% 1. Define a simple callback module to be used for these tests
-%% 1.1. Test it
-%% X 2. Test that inserting a phenotype to a behavior grid that does
-%%    not aready have a phenotype always succeeds.
-%% 3. Test that inserting a phenotype to a behavior grid with an existing
-%%    - less fit - phenotype succeeds
-%% 4. Test that inserting a phenotype to a behavior grid with an existing
-%%    - more fit - phenotype fails
-%% XXX: define the semantics for phenotypes with equal fitness.
 %% 5. Test merging data into archive
 %% 6. Test the data included in the archive with each phenotype.
 %%    (node name and clock value)
 %% 7. Test random lookups??????
 %% 8. Test that get_elites returns what it is supposed to.
-%% X  9. Test that seed_mape leaves the archive with at least one element?
 %% 10. Test get_node_updates and/or get_updates.
-%% 11. Test get_elites. 
-%%     - returns the correct number of phenotypes
-%%     - returns all phenotypes if < N are available.
 
 -define(SIMPLE_CB, simple_example).
 -define(TEST_GRANULARITY, 10).
@@ -62,7 +49,7 @@ seed_test_() ->
     [?_assertEqual(0, length(mape:get_elites(Archive, 5))),
      ?_assert(length(mape:get_elites(Archive1, 5)) >= 1)].
 
-get_elites_length_test() ->
+get_elites_length_test_() ->
     {setup,
      fun empty_mape/0,
      fun destroy_mape/1,
@@ -75,17 +62,47 @@ get_elites_length(Archive) ->
       fun(Phenotype) ->
               mape:insert(Archive, Phenotype, {node(), erlang:make_ref()}, 1)
       end, Phenotypes1),
-    mape:insert(Archive, ?SIMPLE_CB:phenotype(4,4, 2.5)),
+    mape:insert(Archive, ?SIMPLE_CB:phenotype(4,4, 2.5), 
+                {node(), erlang:make_ref()}, 1),
     [?_assertEqual(5, length(mape:get_elites(Archive, 5))),
-     ?_assertEqual(16, length(Phenotypes1)),
-     ?_assertEqual(lists:sort(Phenotypes1),
-                   lists:sort(elite_phenotypes(mape:get_elites(Archive, 16)))),
-     ?_assertEqual(16, length(mape:get_elites(Archive, 100)))
+     ?_assertEqual(17, length(Phenotypes1) + 1),
+     ?_assertEqual(lists:sort(Phenotypes1 ++ [?SIMPLE_CB:phenotype(4, 4, 2.5)]),
+                   lists:sort(elite_phenotypes(mape:get_elites(Archive, 17)))),
+     ?_assertEqual(17, length(mape:get_elites(Archive, 100)))
     ].
 
-%% Test that elites returned by get_elites are actually the elites.
-get_elites_elites_test() ->
-    ?assert(false).
+get_elites_elites_test_() ->
+    {setup,
+     fun empty_mape/0,
+     fun destroy_mape/1,
+     fun get_elites_elites/1}.
+
+get_elites_elites(Archive) ->
+    Phenotypes = [ ?SIMPLE_CB:phenotype(X, Y, rand:uniform(1000)) ||
+                    X <- lists:seq(0, 9),
+                    Y <- lists:seq(0, 9)],
+    lists:foreach(
+      fun(Phenotype) ->
+              mape:insert(Archive, Phenotype, {node(), erlang:make_ref()}, 1)
+      end, Phenotypes),
+    Elites = mape:get_elites(Archive, length(Phenotypes)),
+    ShortPhenotypes = lists:sublist(
+                        lists:reverse(
+                          lists:keysort(2,Phenotypes)), 1, 5),
+    ShortElites = mape:get_elites(Archive, 5),
+    [?_assertEqual(lists:sort(Phenotypes),
+                   lists:sort(elite_phenotypes(Elites))),
+     ?_assertEqual(lists:sort(ShortPhenotypes), 
+                   lists:sort(elite_phenotypes(ShortElites)))].
+    
 
 insert_better_phenotype_test() ->
-    ?assert(false).
+    Archive = empty_mape(),
+    mape:insert(Archive, ?SIMPLE_CB:phenotype(1, 1, 0.0), 
+                {node(), erlang:make_ref()}, 0),
+    mape:insert(Archive, ?SIMPLE_CB:phenotype(1, 1, 2.0),
+                {node(), erlang:make_ref()}, 0),
+    mape:insert(Archive, ?SIMPLE_CB:phenotype(1, 1, 1.0),
+                {node(), erlang:make_ref()}, 0),
+    ?assertEqual([{[1,1], {[1,1], 2.0}}], mape:get_elites(Archive, 1)).
+
